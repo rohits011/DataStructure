2dd5393bea075b1e642345a7ef56d3cd689cc7f1
rohit singh <32682154+rohits011@users.noreply.github.com>
2023-10-20 09:05:12 +0000

    juss kidding

---
+++ dsa/src/main/java/com/rohit/LinearDataStructure/Array/Array.java
@@ -0,0 +1,11 @@
+import javax.swing.plaf.basic.BasicArrowButton;
+
+public class ArrayTut{
+    private Basics basic;
+    public void setBasic(Basics basic){
+        this.basic=basic;
+    }
+    public Basics getBasic(){
+        return this.basic;
+    }
+}
